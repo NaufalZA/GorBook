7a606647dde7b736d9916e2cc758649efb8e20e7
Naufal Zaidan <naufal.zaidan@mhs.itenas.ac.id>
2024-11-14 17:40:19 +0700

    add flutter bricks

---
+++ lib/bricks/Widgets Example/gradient_app_bar_1.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// - - - - - - - - - - - - Instructions - - - - - - - - - - - - - -
+// Place AppBarFb1 inside the app bar property of a Scaffold
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class GradientAppBarFb1 extends StatelessWidget with PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+
+  GradientAppBarFb1({Key? key})
+      : preferredSize = const Size.fromHeight(56.0),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    const primaryColor = Color(0xff4338CA);
+    const secondaryColor = Color(0xff6D28D9);
+    const accentColor = Color(0xffffffff);
+    const backgroundColor = Color(0xffffffff);
+    const errorColor = Color(0xffEF4444);
+
+    return AppBar(
+      title:
+          const Text("Flutter Bricks", style: TextStyle(color: Colors.white)),
+      backgroundColor: primaryColor,
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [primaryColor, secondaryColor],
+            stops: [0.5, 1.0],
+          ),
+        ),
+      ),
+    );
+  }
+}
